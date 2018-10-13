2594740bd2e9e3e8149b106ef4b41f7d5f5fedd6
P Marecki <pmarecki.pro@gmail.com>
2018-10-13 14:26:41 +0200

    klasy usera i serwisu logującego

--- static/js/mainApp.js
+++ static/js/mainApp.js
@@ -22,9 +22,59 @@ app.config(['$routeProvider', '$logProvider', function ($routeProvider, $logProv
     });
 }]);
 
+
+/////////////////////////////////////////////
+
+class LoggedUser {
+    constructor(album, wdauth) {
+        this.album = album;
+        this.wdauth = wdauth;
+    }
+
+    report() {
+        alert(this.album);
+    }
+}
+
+
+//nasza klasa
+class LogonService {
+    //default values
+    constructor() {
+        this.cleanup();
+    }
+
+    login(album, pass) {
+        //call external service
+        console.log('logging in');
+        this.user = new LoggedUser(album, pass);
+        this.loggedIn = true;
+    }
+
+    logout() {
+        console.log('logging out');
+        this.cleanup();
+    }
+
+    cleanup() {
+        this.user = new LoggedUser("", "");
+        this.loggedIn = false;
+    }
+}
+
+
+
+
+
+
 //// COMMON DEFINITIONS
 app.run(function ($rootScope, $window, $http, $location, $timeout, $interval) {
     $rootScope.R = {};
+
+
+    $rootScope.R.logonService = new LogonService();
+    $rootScope.R.user = new LoggedUser("Z1234", "1111");
+
 
     //Global properties
     console.log('Global run');
